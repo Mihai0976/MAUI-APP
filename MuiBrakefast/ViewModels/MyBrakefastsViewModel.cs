35a17b6d9a1911029b809b81cd03f19e7bb47c65
Mihai0976 <mihai.sorinescu@ha.ax>
2024-02-28 21:48:35 +0200

    Add functionality to refresh  the time of the brakefast and add time dinamicly by data binding  change the font size and the  color of the title.  Changed background color   of the AppShell.

--- MuiBrakefast/ViewModels/MyBrakefastsViewModel.cs
+++ MuiBrakefast/ViewModels/MyBrakefastsViewModel.cs
@@ -26,7 +26,7 @@ namespace MauiBrakefast.ViewModels;
                Name: "Vegan All The Way",
                Description:"Everything is Vegan Please join us for a healthy meal",
                StartDateTime: DateTime.UtcNow.AddDays(1),
-               EndDateTime: DateTime.UtcNow.AddDays(1).AddHours(1),
+               EndDateTime: DateTime.UtcNow.AddDays(1).AddHours(2),
                Image: new Uri("https://plus.unsplash.com/premium_photo-1699563104922-139d885755d7?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"),
                Savory: new List<string>{"Bread", "Avocado", "Chery Taomatoes"},
                Sweets: new List<string>{"Vegan Cookie"}),
